c938aff5e7ed5ce32ca363830a4a9c72a7fd9520
arobinsongit <aprobi@gmail.com>
2016-11-19 23:24:55 -0500

    Initial Commit

--- aaSQLToSplunk/Program.cs
+++ aaSQLToSplunk/Program.cs
@@ -1,15 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Timers;
 
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log.config", Watch = true)]
 namespace aaSQLToSplunk
 {
     class Program
     {
+        #region Globals
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static HttpClient _client = new HttpClient();
+
+        //ClientID Guid for Splunk
+        private static Guid _splunkClientID;
+
+        //Setup Timer for reading logs
+        private static Timer _readTimer;
+
+        // Cache off the last read record
+        private static object _lastRecordTransmitted;
+        
+        //Runtime Options Object
+        private static OptionsStruct _runtimeOptions;
+
+        // Global SQL Connection
+        private static SqlConnection _connection;
+
+        #endregion
+        
         static void Main(string[] args)
         {
+
+            // Setup logging
+            log4net.Config.BasicConfigurator.Configure();
+
+            try
+            {
+                _runtimeOptions = JsonConvert.DeserializeObject<OptionsStruct>(System.IO.File.ReadAllText("options.json"));
+
+
+            }
+            catch(Exception ex)
+            {
+                log.Error(ex);
+            }
+
+            
+            
+        }
+
+        private static bool SetupSqlConnection(string connectionString)
+        {
+            _connection = new SqlConnection
+
+            using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=SplunkTest;Integrated Security=SSPI"))
+            {
+                connection.Open();
+            }
         }
     }
 }
---
+++ aaSQLToSplunk/Struct/OptionsStruct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace aaSQLToSplunk
+{
+    public class OptionsStruct
+    {
+        public int ReadInterval = 5000;
+        public int MaximumReadInterval = 50000;
+        public ulong MaxUnreadRecords = 1000;
+        public string SQLConnectionString = "";
+        public string SQLQuery = "";
+        public string SplunkBaseAddress = "http://localhost:8088";
+        public string AuthorizationToken = "0000-0000-0000-0000-0000";
+        public Guid ClientID = Guid.NewGuid();
+    }
+}
